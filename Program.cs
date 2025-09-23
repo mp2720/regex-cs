3e159c6a6ca45d409df92bf06be1b0fe12dab0fa
Salomakhin Timur <mahintim2@gmail.com>
2025-09-23 20:50:23 +0300

    add solution

--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Regex;
-using Regex.Graphviz;
-
-var regexToNFA = new RegexToNFA(new List<(char, CharClass)>());
-var nfa = regexToNFA.Convert("(0|0x[0-9a-fA-f]+|[1-9][0-9]*|0o[0-7]+|0b[01]+)?");
-new NFAToGraphviz().Convert(Console.Out, nfa);
