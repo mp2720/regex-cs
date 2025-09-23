6a63f9298e34a708003078f304b9db3f32692336
Salomakhin Timur <mahintim2@gmail.com>
2025-09-23 20:50:36 +0300

    add scanner

---
+++ regex/Program.cs
@@ -0,0 +1,12 @@
+﻿using Regex;
+using Regex.Graphviz;
+using Regex.NFA;
+
+using System.Text;
+
+// var nfa = regexToNFA.Convert("(0|0x[0-9a-fA-f]+|[1-9][0-9]*|0o[0-7]+|0b[01]+)?");
+// var nfa = regexToNFA.Convert("[a-zA-Z_][a-zA-Z_0-9]*");
+var regexToNFA = new RegexToNFA([]);
+var nfa = regexToNFA.Convert("(a+)*bz");
+var exec = new Scanner(nfa);
+Console.WriteLine(exec.Match(Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz")));
---
+++ regex/Scanner.cs
@@ -0,0 +1,88 @@
+namespace Regex.NFA
+{
+    /// <summary>
+    /// Primitive Thompson-style NFA scanner. Each step transitions to all possible states are processed.
+    /// Thus the runtime performance is O(m*n) (m - size of NFA, n - size of input).
+    /// </summary>
+    public class Scanner
+    {
+        private class ScanState
+        {
+            public readonly State state;
+            public int byteIndex = 0;
+
+            public ScanState(State state, int byteIndex)
+            {
+                this.state = state;
+                this.byteIndex = byteIndex;
+            }
+        }
+
+        private readonly Automaton nfa;
+
+        public Scanner(Automaton nfa)
+        {
+            this.nfa = nfa;
+        }
+
+        private bool DoMatch(byte c, CharClass cl)
+        {
+            bool matchesRanges = cl.Ranges.Any(range => range.From <= c && c <= range.To);
+            return matchesRanges == !cl.Inverted;
+        }
+
+        public bool Match(byte[] input)
+        {
+            var scanStates = new ScanState?[nfa.States.Count];
+            var newScanStates = new ScanState?[nfa.States.Count];
+            bool hasNewStates = true;
+
+            scanStates[nfa.Source.Index] = new ScanState(nfa.Source, 0);
+
+            while (hasNewStates)
+            {
+                hasNewStates = false;
+                for (int i = 0; i < scanStates.Length; ++i)
+                {
+                    var ss = scanStates[i];
+                    if (ss == null)
+                        continue;
+
+                    scanStates[i] = null;
+
+                    if (ss.state.Index == nfa.Sink.Index)
+                    {
+                        if (ss.byteIndex == input.Length)
+                            return true; // match!
+
+                        continue; // we're in the sink, but there're unmatched characters
+                    }
+
+                    foreach (var trans in ss.state.Transitions)
+                    {
+                        if (trans.Condition != null)
+                        {
+                            // non-ε
+
+                            if (ss.byteIndex == input.Length)
+                                // no characters left but we are not in the sink
+                                continue;
+
+                            if (!DoMatch(input[ss.byteIndex], trans.Condition))
+                                continue;
+
+                            ss.byteIndex++;
+                        }
+
+                        newScanStates[trans.To.Index] = new ScanState(trans.To, ss.byteIndex);
+                        hasNewStates = true;
+                    }
+                }
+
+                (scanStates, newScanStates) = (newScanStates, scanStates);
+            }
+
+            return false;
+        }
+    }
+}
